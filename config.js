f8f33051182309800bf44fb2884589ff6f90bd3c
方泽鹏 <2687026254@qq.com>
2026-07-19 10:48:44 +0800

    Configure Supabase guestbook project

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.FZ_BLOG_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://ydydxfoidbeurjfphafn.supabase.co",
+  supabaseAnonKey: "sb_publishable_pO3ZaKsv4w8VnbQhJ2UJ-A_-IDDEC5g",
   supabaseStorageBucket: "guestbook"
 };
